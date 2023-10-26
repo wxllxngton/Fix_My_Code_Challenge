ea31d6c3d2219031dd8bf8432aa61520feb2674e
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2023-10-26 12:13:17 +0300

    Update 0-fizzbuzz.py

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-""" 
-FizzBuzz
+""" FizzBuzz
 """
 import sys
-
 
 def fizzbuzz(n):
     """
@@ -18,16 +16,15 @@ def fizzbuzz(n):
 
     tmp_result = []
     for i in range(1, n + 1):
-        if (i % 3) == 0 and (i % 5) == 0:
+        if (i % 3 == 0) and (i % 5 == 0):
             tmp_result.append("FizzBuzz")
-        elif (i % 3) == 0:
+        elif i % 3 == 0:
             tmp_result.append("Fizz")
-        elif (i % 5) == 0:
+        elif i % 5 == 0:
             tmp_result.append("Buzz")
         else:
             tmp_result.append(str(i))
-    print(" ".join(tmp_result))
-
+    print(" ".join(tmp_result)
 
 if __name__ == '__main__':
     if len(sys.argv) <= 1:
